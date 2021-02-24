422e54a4d60e2ae2c70a6cb39a58bac2679a97d6
0x454447415244 <contact@edgard.net>
2021-02-24 05:14:00 +0200

    update

--- index.js
+++ index.js
@@ -92,46 +92,6 @@ class CustomCrop extends Component {
         });
     }
 
-    /*
-    RCT_EXPORT_METHOD(crop:(NSDictionary *)points imageUri:(NSString *)imageUri callback:(RCTResponseSenderBlock)callback)
-    {
-        CFDataRef dataRef = (__bridge CFDataRef)[NSData dataWithContentsOfFile:imageUri];
-        CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData(dataRef);
-        CGImageRef intialImageRef = CGImageCreateWithJPEGDataProvider(imgDataProvider, NULL, true, kCGRenderingIntentDefault);
-        CIImage *intialImage = [CIImage imageWithCGImage:intialImageRef];
-        intialImage = [intialImage imageByApplyingOrientation:kCGImagePropertyOrientationRight];
-        
-        NSMutableDictionary *rectangleCoordinates = [NSMutableDictionary new];
-        
-        CGPoint topLeft = CGPointMake([points[@"topLeft"][@"x"] floatValue], [points[@"topLeft"][@"y"] floatValue]);
-        CGPoint topRight = CGPointMake([points[@"topRight"][@"x"] floatValue], [points[@"topRight"][@"y"] floatValue]);
-        CGPoint bottomLeft = CGPointMake([points[@"bottomLeft"][@"x"] floatValue], [points[@"bottomLeft"][@"y"] floatValue]);
-        CGPoint bottomRight = CGPointMake([points[@"bottomRight"][@"x"] floatValue], [points[@"bottomRight"][@"y"] floatValue]);
-        
-        CGFloat height = intialImage.extent.size.height;
-        topLeft = [self cartesianForPoint:topLeft height:height];
-        topRight = [self cartesianForPoint:topRight height:height];
-        bottomLeft = [self cartesianForPoint:bottomLeft height:height];
-        bottomRight = [self cartesianForPoint:bottomRight height:height];
-
-        rectangleCoordinates[@"inputTopLeft"] = [CIVector vectorWithCGPoint:topLeft];
-        rectangleCoordinates[@"inputTopRight"] = [CIVector vectorWithCGPoint:topRight];
-        rectangleCoordinates[@"inputBottomLeft"] = [CIVector vectorWithCGPoint:bottomLeft];
-        rectangleCoordinates[@"inputBottomRight"] = [CIVector vectorWithCGPoint:bottomRight];
-
-        CIImage * croppedImage = [intialImage imageByApplyingFilter:@"CIPerspectiveCorrection" withInputParameters:rectangleCoordinates];
-        
-        CIContext *context = [CIContext contextWithOptions:nil];
-        CGImageRef croppedref = [context createCGImage:croppedImage fromRect:[croppedImage extent]];
-        UIImage *image = [UIImage imageWithCGImage:croppedref];
-        
-        NSData *imageToEncode = UIImageJPEGRepresentation(image, 0.8);
-        callback(@[[NSNull null], @{@"image": [imageToEncode base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength]}]);
-        
-        CGImageRelease(croppedref);
-    }
-    */
-
    crop() {
 
         let _topLeft = this.viewCoordinatesToImageCoordinates(this.state.topLeft)
